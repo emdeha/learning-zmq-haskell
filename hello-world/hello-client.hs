a25f7ec2b683c635b05397528ad0ed57c5b08eb5
cpi-cecko <cpi.cecko@gmail.com>
2014-08-30 11:20:07 +0300

    implements simple client-server in zeroMQ

--- hello-world/hello-client.hs
+++ hello-world/hello-client.hs
@@ -1,3 +1,9 @@
+-- |
+-- Simple client in Haskell
+-- Gets ID assigned by the server
+-- Sends request
+-- Received response
+-- |
 module Main where
 
 import System.ZMQ4.Monadic
@@ -10,8 +16,13 @@ main =
         liftIO $ putStrLn "Connecting to Hello World server..."
         reqSocket <- socket Req
         connect reqSocket "tcp://localhost:5555"
+
+        liftIO $ putStrLn "Getting ID"
+        send reqSocket [] (pack $ "id")
+        id <- receive reqSocket
+
         forM_ [1..10] $ \i -> do
             liftIO $ putStrLn $ unwords ["Sending request", show i]
-            send reqSocket [] (pack "Hello")
+            send reqSocket [] (pack $ "Hello " ++ (show i) ++ " | ID: " ++ (unpack id))
             reply <- receive reqSocket
             liftIO $ putStrLn $ unwords ["Received reply:", unpack reply]
---
+++ hello-world/hello-server.hs
@@ -0,0 +1,36 @@
+-- |
+-- Simple server in Haskell
+-- Assigns IDs to every new client
+-- Receives data
+-- Sends response
+-- |
+module Main where
+
+import System.ZMQ4.Monadic
+import System.Random
+import Control.Monad (forever)
+import Data.ByteString.Char8 (pack, unpack)
+import Control.Concurrent (threadDelay)
+
+
+rand :: IO Int
+rand = liftIO $ getStdRandom (randomR (0, maxBound))
+
+main :: IO ()
+main =  
+    runZMQ $ do
+        liftIO $ putStrLn "Establishing server..."
+        repSocket <- socket Rep
+        bind repSocket "tcp://*:5555"
+
+        forever $ do
+            msg <- receive repSocket
+            (liftIO . putStrLn . unwords) ["Received request: ", unpack msg]
+
+            liftIO $ threadDelay (1 * 1000 * 1000)
+
+            if unpack msg == "id"
+            then do 
+                id <- liftIO $ rand
+                send repSocket [] (pack $ show id)
+            else send repSocket [] (pack "World")
