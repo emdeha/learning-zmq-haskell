ac5bb1e8e294d5ebfc33efe050db7bb46e26c004
cpi-cecko <cpi.cecko@gmail.com>
2014-09-03 07:59:29 +0300

    implements sink which kills its workers when they are done

--- req-broker/rrbroker.hs
+++ req-broker/rrbroker.hs
@@ -6,6 +6,9 @@
 module Main where
 
 import System.ZMQ4.Monadic
+import Control.Monad (forever, when)
+import Control.Applicative ((<$>))
+import Data.ByteString.Char8 (unpack, pack)
 
 main :: IO ()
 main =  
@@ -15,4 +18,22 @@ main =
         backend <- socket Dealer
         bind backend "tcp://*:5560"
 
+        -- |
+        -- Using self-made proxy
+        -- forever $ do
+        --     poll (-1)
+        --          [ Sock frontend [In] (Just $ processEvts fronend)
+        --          , Sock backend [In] (Just $ processEvts backend)]
+        -- |
+
+        -- |
+        -- Using the proxy function
         proxy frontend backend Nothing
+        -- |
+
+--processEvts :: (Receiver a) => Socket a -> [Event] -> IO ()
+--processEvts sock evts =
+--    when (In `elem` evts) $ d0
+--        msg <- unpack <$> receive sock
+--        send backend msg []
+--        putStrLn $ unwords ["Sent ", msg]
