6ce9e23a87659a40f93e3e46854aa79d107dda9f
cpi-cecko <cpi.cecko@gmail.com>
2014-09-03 08:19:03 +0300

    modifies the broker so it implements its own proxy function

--- req-broker/rrbroker.hs
+++ req-broker/rrbroker.hs
@@ -5,35 +5,41 @@
 -- |
 module Main where
 
-import System.ZMQ4.Monadic
+import System.ZMQ4
 import Control.Monad (forever, when)
 import Control.Applicative ((<$>))
 import Data.ByteString.Char8 (unpack, pack)
 
 main :: IO ()
 main =  
-    runZMQ $ do
-        frontend <- socket Router
-        bind frontend "tcp://*:5559"
-        backend <- socket Dealer
-        bind backend "tcp://*:5560"
+    withContext $ \ctx ->
+        withSocket ctx Router $ \frontend ->
+        withSocket ctx Dealer $ \backend -> do
+            bind frontend "tcp://*:5559"
+            bind backend "tcp://*:5560"
 
-        -- |
-        -- Using self-made proxy
-        -- forever $ do
-        --     poll (-1)
-        --          [ Sock frontend [In] (Just $ processEvts fronend)
-        --          , Sock backend [In] (Just $ processEvts backend)]
-        -- |
+            -- |
+            -- Using self-made proxy
+            doProxy frontend backend
+            -- |
 
-        -- |
-        -- Using the proxy function
-        proxy frontend backend Nothing
-        -- |
+            -- |
+            -- Using the proxy function
+            -- proxy frontend backend Nothing
+            -- |
 
---processEvts :: (Receiver a) => Socket a -> [Event] -> IO ()
---processEvts sock evts =
---    when (In `elem` evts) $ d0
---        msg <- unpack <$> receive sock
---        send backend msg []
---        putStrLn $ unwords ["Sent ", msg]
+doProxy :: (Sender a, Receiver a, Sender b, Receiver b) => Socket a -> Socket b -> IO ()
+doProxy frontend backend = do
+    forever $ do
+        [fr, back] <- poll (-1) [ Sock frontend [In] Nothing 
+                                , Sock backend [In] Nothing]
+
+        when (In `elem` fr) $ do
+            msg <- receive frontend
+            putStrLn $ (unpack msg) 
+            send backend [] msg
+
+        when (In `elem` back) $ do
+            msg <- receive backend
+            putStrLn $ (unpack msg)
+            send frontend [] msg
