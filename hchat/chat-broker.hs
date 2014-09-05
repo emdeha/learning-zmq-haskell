f80e656bb8e6775af369b3f0e0f2c1f77d986dd9
cpi-cecko <cpi.cecko@gmail.com>
2014-09-05 07:39:14 +0300

    the chat now manages to send some messages

--- hchat/chat-broker.hs
+++ hchat/chat-broker.hs
@@ -1,18 +1,19 @@
 -- |
 -- Pub/Sub broker
--- Binds Router to tcp://*:5559
--- Binds Dealer to tcp://*:5560
 -- |
 module Main where
 
 import System.ZMQ4
+import Data.ByteString.Char8 (pack)
 
 main :: IO ()
 main =  
     withContext $ \ctx ->
-        withSocket ctx Router $ \frontend ->
-        withSocket ctx Dealer $ \backend -> do
-            bind frontend "tcp://*:5510"
-            bind backend "tcp://*:5520"
+        withSocket ctx Sub $ \frontend ->
+        withSocket ctx Pub $ \backend -> do
+            bind frontend "tcp://*:5520"
+            bind backend "tcp://*:5510"
+
+            subscribe frontend (pack "")
 
             proxy frontend backend Nothing
