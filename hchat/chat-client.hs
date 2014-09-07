e07627539df7411099499f2637bd0a8531abe108
cpi-cecko <cpi.cecko@gmail.com>
2014-09-07 17:30:08 +0300

    makes the chat-client async

--- hchat/chat-client.hs
+++ hchat/chat-client.hs
@@ -6,38 +6,51 @@
 -- |
 module Main where
 
-import System.ZMQ4
+import System.ZMQ4.Monadic
+import System.IO (hSetBuffering, stdout, BufferMode(..))
 import Control.Monad (forever)
 import Data.ByteString.Char8 (unpack, pack)
 
 
-requestId :: Context -> IO String
-requestId ctx =
-    withSocket ctx Req $ \id_sock -> do
-        connect id_sock "tcp://localhost:6000"
-        send id_sock [] (pack "gid")        
-        id <- receive id_sock
-        return $ unpack id
+requestId :: ZMQ z String
+requestId = do
+    id_sock <- socket Req
+    connect id_sock "tcp://localhost:6000"
+    send id_sock [] (pack "gid")        
+    id <- receive id_sock
+    return $ unpack id
 
+receiveMessage :: ZMQ z ()
+receiveMessage = do
+    in_sock <- socket Sub
+
+    connect in_sock "tcp://localhost:5510" 
+
+    liftIO $ putStr "partner? "
+    partner <- liftIO $ getLine
+    subscribe in_sock (pack partner)
+
+    forever $ do
+        msg <- receive in_sock
+        liftIO $ putStrLn (unpack msg)
+
+sendMessage :: ZMQ z ()
+sendMessage = do
+    out_sock <- socket Pub
+    connect out_sock "tcp://localhost:5520"
+
+    id <- requestId
+    liftIO $ hSetBuffering stdout NoBuffering
+    liftIO $ putStrLn $ "id received: " ++ id
+
+    forever $ do
+        resp <- liftIO $ getLine
+        send out_sock [SendMore] (pack id)
+        send out_sock [] (pack resp)
+        
 
 main :: IO ()
 main = 
-    withContext $ \ctx ->
-        withSocket ctx Pub $ \out_sock ->
-        withSocket ctx Sub $ \in_sock -> do
-            connect out_sock "tcp://localhost:5520"
-            connect in_sock "tcp://localhost:5510" 
-
-            id <- requestId ctx
-            putStrLn $ "id received: " ++ id
-
-            putStr "partner? "
-            partner <- getLine
-            subscribe in_sock (pack partner)
-
-            forever $ do
-                resp <- getLine
-                send out_sock [SendMore] (pack id)
-                send out_sock [] (pack resp)
-                msg <- receive in_sock
-                putStrLn (unpack msg)
+    runZMQ $ do
+        async sendMessage
+        async receiveMessage
