fd67ad96a2090f1504b601311df651ada4342ed0
cpi-cecko <gogo_rus@abv.bg>
2014-10-13 10:29:13 +0300

    adds time getting functions to ZHelpers

--- ZHelpers.hs
+++ ZHelpers.hs
@@ -4,6 +4,9 @@ import System.ZMQ4.Monadic
 
 import Numeric (showHex)
 
+import Data.Time.Clock
+import Data.Time.LocalTime
+import Data.Fixed
 import Data.ByteString.Char8 (pack, unpack)
 import Data.Char (ord)
 import Text.Printf (printf)
@@ -46,3 +49,15 @@ genUniqueId = do
 
 prettyPrint :: B.ByteString -> String
 prettyPrint = concatMap (`showHex` "") . B.unpack
+
+-- Returns the current *local* time in milliseconds
+currentTime_ms :: IO Integer
+currentTime_ms = do
+    currTime <- getCurrentTime
+    let time_secs = todSec . localTimeOfDay . utcToLocalTime utc $ currTime
+    return $ (resolution $ time_secs) * 1000
+
+nextHeartbeatTime_ms :: Integer -> IO Integer
+nextHeartbeatTime_ms heartbeatInterval_ms = do
+    currTime <- currentTime_ms
+    return $ currTime + heartbeatInterval_ms
