3d32c803c31363dbf300182a9660b2b72bd2fc3b
cpi-cecko <gogo_rus@abv.bg>
2014-11-25 18:49:39 +0200

    implements s_brokerPurge

--- reliable-req-rep/mdbroker.hs
+++ reliable-req-rep/mdbroker.hs
@@ -9,10 +9,13 @@ import System.ZMQ4
 import ZHelpers
 import MDPDef
 
+import Control.Monad.Trans.State
+import Control.Monad.IO.Class (liftIO)
 import Control.Exception (bracket)
-import Control.Monad (forM_, mapM_, when)
-import Data.Map.Strict as Map
+import Control.Monad (forever, forM_, mapM_, foldM, when)
 import Data.ByteString.Char8 (pack, unpack, empty, ByteString(..))
+import qualified Data.Map.Strict as M
+import qualified Data.List as L (partition)
 
 heartbeatLiveness = 1
 heartbeatInterval = 2500
@@ -23,27 +26,24 @@ data Broker = Broker {
     , bSocket :: Socket Router
     , verbose :: Bool
     , endpoint :: String
-    , services :: Map.Map String Service
-    , workers :: Map.Map String Worker
+    , services :: M.Map String Service
+    , workers :: M.Map String Worker
     , bWaiting :: [Worker]
     , heartbeatAt :: Integer
     }
 
 data Service = Service {
-      sBroker :: Broker
-    , name :: String
+      name :: String
     , requests :: [ByteString]
     , sWaiting :: [Worker]
     , workersCount :: Int
     }
 
 data Worker = Worker {
-      wBroker :: Broker
-    , id :: ByteString
+      wId :: ByteString
     , identityFrame :: [ByteString]
-    , service :: Maybe Service
     , expiry :: Integer
-    }
+    } deriving (Eq)
 
 
 withBroker :: Bool -> (Broker -> IO a) -> IO a
@@ -61,8 +61,8 @@ s_brokerNew verbose = do
     return Broker { ctx = ctx
                   , bSocket = bSocket
                   , verbose = verbose
-                  , services = Map.empty
-                  , workers = Map.empty
+                  , services = M.empty
+                  , workers = M.empty
                   , bWaiting = []
                   , heartbeatAt = nextHeartbeat
                   , endpoint = []
@@ -79,12 +79,33 @@ s_brokerBind broker endpoint = do
     putStrLn $ "I: MDP broker/0.2.0 is active at " ++ endpoint
 
 -- Processes READY, REPLY, HEARTBEAT, or DISCONNECT worker message
-s_brokerWorkerMsg :: Broker -> ByteString -> [ByteString] -> IO Broker
 s_brokerWorkerMsg = undefined
 
 s_brokerClientMsg = undefined
 
-s_brokerPurge = undefined
+s_brokerPurge :: Broker -> IO Broker
+s_brokerPurge broker = do
+    currTime <- currentTime_ms
+    let (toPurge, rest) = L.partition (\worker -> currTime > expiry worker)
+                                      (bWaiting broker)
+        leftInMap       = M.filterWithKey (isPurgedKey toPurge) (workers broker)
+        purgedServices  = purgeWorkersFromServices toPurge (services broker)
+    mapM_ (flip s_workerDelete False) toPurge
+    return broker { bWaiting = rest 
+                  , workers = leftInMap 
+                  }
+  where isPurgedKey toPurge key _ = 
+            key `notElem` (map (unpack . wId) toPurge)
+
+        purgeWorkersFromServices workers services = 
+            M.map purge services
+          where purge service =
+                    let (toPurge, rest) = L.partition (\worker -> worker `elem` workers)
+                                                      (sWaiting service)
+                    in  service { sWaiting = rest
+                                , workersCount = (workersCount service) - (length toPurge)
+                                }
+           
 
 
 -- Service functions
@@ -98,9 +119,10 @@ s_serviceDispatch = undefined
 -- Worker functions
 s_workerRequire = undefined
 
-s_workerDelete = undefined
-
-s_workerDestroy = undefined
+s_workerDelete :: Worker -> Bool -> IO () 
+s_workerDelete worker isDisconnect =
+    when isDisconnect $ do
+        s_workerSend worker mdpwDisconnect Nothing Nothing
 
 s_workerSend = undefined
 
@@ -109,40 +131,47 @@ s_workerWaiting = undefined
 
 -- Main. Create a new broker and process messages on its socket.
 main :: IO ()
-main =
+main = undefined
+{-
     withBroker True $ \broker -> do
         s_brokerBind broker "tcp://*:5555"
+        evalState doPoll broker-- (foreverS doPoll) broker
+      where --doPoll :: State Broker (IO ())
+            doPoll =
+                forever $ do
+                    broker <- get
+                    [evts] <- poll (fromInteger heartbeatInterval) 
+                                   [Sock (bSocket broker) [In] Nothing]
 
-        doPoll broker
-      where doPoll :: Broker -> IO ()
-            doPoll broker = do
-                [evts] <- poll (fromInteger heartbeatInterval) [Sock (bSocket broker) [In] Nothing]
+                    when (In `elem` evts) $ do
+                        msg <- liftIO $ receiveMulti $ bSocket broker
 
-                newBroker <- if (In `elem` evts) 
-                             then do
-                                  msg <- receiveMulti $ bSocket broker
+                        when (verbose broker) $ do
+                            liftIO $ putStrLn "I: received message: " >> dumpMsg msg
 
-                                  when (verbose broker) $ do
-                                      putStrLn "I: received message: "
-                                      dumpMsg msg
+                        let sender = msg !! 0
+                            empty = msg !! 1
+                            header = msg !! 2
+                            msg' = drop 3 msg
+                        case header of
+                            head | head == mdpcClient -> s_brokerClientMsg sender msg'
+                                 | head == mdpwWorker -> s_brokerWorkerMsg sender msg'
+                                 | otherwise          -> do liftIO $ putStrLn "E: Invalid message"
+                                                            liftIO $ dumpMsg msg'
+     
+                    currTime <- liftIO $ currentTime_ms
+                    when (currTime > heartbeatAt broker) $ do
+                        s_brokerPurge
+                        forM_ (bWaiting broker) $ \worker -> do
+                            s_workerSend worker mdpwHeartbeat Nothing Nothing
+                        nextHeartbeat <- liftIO $ nextHeartbeatTime_ms heartbeatInterval
+                        newBroker <- get
+                        put newBroker { heartbeatAt = nextHeartbeat }
 
-                                  let sender = msg !! 0
-                                      empty = msg !! 1
-                                      header = msg !! 2
-                                  case header of
-                                      head | head == mdpcClient -> s_brokerClientMsg broker sender msg
-                                           | head == mdpwWorker -> s_brokerWorkerMsg broker sender msg
-                                           | otherwise          -> do putStrLn "E: Invalid message"
-                                                                      dumpMsg msg
-                                                                      return broker
-                              else return broker   
-                
-                currTime <- currentTime_ms
-                if currTime > heartbeatAt newBroker
-                then do
-                    newBroker' <- s_brokerPurge newBroker
-                    forM_ (bWaiting newBroker') $ \worker -> do
-                        s_workerSend worker mdpwHeartbeat Nothing Nothing
-                    nextHeartbeat <- nextHeartbeatTime_ms heartbeatInterval
-                    doPoll newBroker' { heartbeatAt = nextHeartbeat }
-                else doPoll newBroker
+foreverS :: State s a -> State s a
+foreverS body =
+    do modify (execState body)
+       foreverS body
+  where execState :: State s a -> s -> s
+        execState mv init_st = snd (runState mv init_st)
+-}
