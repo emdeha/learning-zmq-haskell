f8f9ba721abf0ab859a00aa34004816b45e85590
cpi-cecko <gogo_rus@abv.bg>
2014-12-10 12:28:39 +0200

    implements worker taks

--- reliable-req-rep/tripping.hs
+++ reliable-req-rep/tripping.hs
@@ -7,12 +7,14 @@ import System.ZMQ4
 import ZHelpers 
 
 import Control.Concurrent
-import Control.Monad (forM_)
-import Data.ByteString.Char8 (pack, unpack, empty, ByteString(..))
+import Control.Monad (forM_, forever)
+import Data.ByteString.Char8 (pack)
+
 
 clientTask :: Context -> Socket Req -> IO ()
 clientTask ctx pipe =
     withSocket ctx Dealer $ \client -> do
+        connect client "tcp://localhost:5555"
         putStrLn "Setting up test..."
         threadDelay $ 100 * 1000
 
@@ -34,6 +36,16 @@ clientTask ctx pipe =
         putStrLn $ " " ++ (show $ (1000 * 10000) / (fromInteger elapsed_ms)) ++ " calls/second"
 
         send pipe [] (pack "done")
+
+worker_task :: IO ()
+worker_task = 
+    withContext $ \ctx -> do
+        withSocket ctx Dealer $ \worker -> do
+            connect worker "tcp://localhost:5556"
+
+            forever $ do
+                msg <- receive worker
+                send worker [] msg
 
 main :: IO ()
 main = undefined
